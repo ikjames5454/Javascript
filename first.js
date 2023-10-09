face5144f72475010c69a651b3dab751bbd263fa
ikjames5454 <ikennajames03@gmail.com>
2023-10-09 15:06:18 +0100

    add

---
+++ first.js
@@ -0,0 +1,21 @@
+// console.log('Hello World');
+// console.log(2 + 2)
+// let array = [[3,4,6],[5,2,8]];
+
+// console.table(array)
+
+let array  = []
+array.push(["car", 2000])
+array.push(["Truck",  500])
+array.push(["Bike",6500])
+array.push(["scooter", 1500])
+array[0].push(["Toyota","Nissan"])
+array[1].push(["Ford"])
+array[2].push(["Honda"])
+
+
+console.table(array)
+
+
+
+
